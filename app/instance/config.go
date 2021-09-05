a0d6d1593f4b451daedd3be2234869baf2dd012a
jetking <cnjetking@gmail.com>
2021-09-05 11:46:22 +0800

    config 中接受 GO_PROJECT_PATH 环境变量以便于调试

--- app/instance/config.go
+++ app/instance/config.go
@@ -1,13 +1,20 @@
 package instance
 
 import (
+	"fmt"
 	"github.com/spf13/viper"
+	"os"
 )
 
 func init() {
-	viper.AddConfigPath(".")
+	projectPath := os.Getenv("GO_PROJECT_PATH")
+	if projectPath == "" {
+		projectPath = "."
+	}
+	viper.AddConfigPath(projectPath)
 	viper.SetConfigName("config")
+
 	if err := viper.ReadInConfig(); err != nil {
-		//panic(fmt.Errorf("Fatal error config file: %s \n", err))
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
 }
